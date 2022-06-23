345e87921ccf75ce13076c18ca08c31c51a4628f
FarrukhAdeel67 <m.farrukhadeel@gmail.com>
2022-06-23 20:14:22 +0500

    third commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const studentRouter = require('./routes/student');
 app.use("/api/students", studentRouter);
 
 mongoose.connect("mongodb+srv://farrukhadeel:17301pker@cluster0.ysuiv.mongodb.net/test").then(result => {
-    console.log("Database connected Successfully");
+    console.log("Database connected Successfully!");
 }).catch(err => {
     console.log(err)
 
