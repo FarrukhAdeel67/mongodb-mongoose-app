8a451eb7ff447011aaa25fa668dcd0814370d829
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-13 02:06:37 -0700

    updated

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(express.json());
 
 const studentRouter = require('./routes/student');
 app.use("/api/students", studentRouter);
-
-mongoose.connect("mongodb+srv://farrukhadeel:17301pker@cluster0.ysuiv.mongodb.net/test").then(result => {
+//password is removed from connection
+mongoose.connect("mongodb+srv://farrukhadeel:@cluster0.ysuiv.mongodb.net/test").then(result => {
     console.log("Database connected Successfully!");// this is for connection with database
 }).catch(err => {
     console.log(err)
