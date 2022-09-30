5460603d1912f40b355468f140e6c631e0b9daaa
Farrukh Adeel <81711287+FarrukhAdeel67@users.noreply.github.com>
2022-09-30 22:24:28 +0500

    Update student.js

--- routes/student.js
+++ routes/student.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const studentController = require("../controllers/student")
+
 router.post("/", studentController.post);
 
 router.get("/", studentController.getAll);
